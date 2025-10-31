3c40565965ed2ff20dd62188679e6fd047137b57
Gabriel <132002025+Gabrielt312@users.noreply.github.com>
2025-10-30 21:36:34 -0300

    Add files via upload

---
+++ Aula 7/js/script.js
@@ -0,0 +1,15 @@
+function ligar(){
+    document.getElementById('lamp').src = 'img/pic_bulboff.gif';
+}
+
+function desligar(){
+    document.getElementById('lamp').src = 'img/pic_bulbon.gif';
+
+}   
+
+function onOff(){
+    if(lamp2 != lamp2){
+    document.getElementById('lamp2').src = 'img/pic_bulboff.gif';
+    }else{
+    }
+}
